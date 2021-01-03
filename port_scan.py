6e76ed8fa65010e8abc7b1ad8e5d765399d654d3
rootshooter <75587096+rootshooter@users.noreply.github.com>
2021-01-03 08:58:29 -0500

    Update port_scan.py

--- port_scan.py
+++ port_scan.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 
-import socket
+import socket, sys
 from termcolor import colored
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = input("Please enter target IP address: ")
 
-for port in range(1,65536):
-	try:
-		s.connect((host, port))
+try:
+	for port in range(1,65536):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		r = s.connect_ex((host, port))
+		if r == 0:
+			print(colored("[+] Port %s is open!" % str(port), "green"))
 		s.close()
-		print(colored("[+] Port %s is open!" % str(port), "green"))
-	except:
-		continue
+	else:
+	    	print(colored("[*] Scan complete!", "green"))
+except socket.error:
+	print(colored("[-] Could not connect to the server!"), "red")
+	sys.exit(0)
+except KeyboardInterrupt:
+	print(colored("[-] Scanning cancelled!"), "red")
+	sys.exit(0)
